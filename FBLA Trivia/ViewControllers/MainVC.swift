ac7d8627e18195674f72ebe6ed6ffff818a9b15b
Tanner York <TannerYork@MacBook-Pro-4.local>
2019-01-15 14:23:24 -0600

    Setup the basic layout of the app.

---
+++ FBLA Trivia/ViewControllers/MainVC.swift
@@ -0,0 +1,28 @@
+//
+//  MainVC.swift
+//  FBLA Trivia
+//
+//  Created by Tanner York on 1/15/19.
+//  Copyright © 2019 Tanner York. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
