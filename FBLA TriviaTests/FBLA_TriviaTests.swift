5ad94bbd1a3b697821a1cd67c75f24ca1c06865b
Tanner York <TannerYork@macbook-pro-4.local>
2019-01-15 11:26:34 -0600

    Initial Commit

---
+++ FBLA TriviaTests/FBLA_TriviaTests.swift
@@ -0,0 +1,34 @@
+//
+//  FBLA_TriviaTests.swift
+//  FBLA TriviaTests
+//
+//  Created by Tanner York on 1/15/19.
+//  Copyright © 2019 Tanner York. All rights reserved.
+//
+
+import XCTest
+@testable import FBLA_Trivia
+
+class FBLA_TriviaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
